a3d45ea9e851a6ffd70f4cc9147b24a066e3f89e
Anikin Luke Abales <47096239+ph-fox@users.noreply.github.com>
2022-08-14 16:40:22 +0800

    Create xorify.py

---
+++ xorify.py
@@ -0,0 +1,16 @@
+
+text ="fuckyou"
+from_string = []
+for char in text:
+	from_string.append(ord(char))
+
+from_int = []
+for num in from_string:
+	from_int.append(num^13)
+
+text = ""
+for char in from_int:
+	text+=str(chr(char))
+
+print(text)
+
