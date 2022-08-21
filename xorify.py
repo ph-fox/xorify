1a481a9369cfc235eec426437742413ab0b61951
Anikin Luke Abales <47096239+ph-fox@users.noreply.github.com>
2022-08-21 16:38:39 +0800

    Update xorify.py

--- xorify.py
+++ xorify.py
@@ -1,12 +1,5 @@
+def xorify(text, num):
+	converted = ''.join([chr(x) for x in ([ord(x)^int(num) for x in text])])
+	print(converted)
 
-text = "label"
-
-msg_int = []
-for char in text:
-	msg_int.append(ord(char)^13)
-
-text = ""
-for integers in msg_int:
-	text+=chr(integers)
-	
-print(text)
+xorify('label',13)
