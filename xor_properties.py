bdc68d52943e8aa930a517a69874f5d050acd91f
Nui <47096239+ph-fox@users.noreply.github.com>
2025-02-25 02:09:51 +0800

    Removed key2 xor_properties.py
    I removed KEY2 cuz it cancels out due to XOR properties, so only KEY1, KEY3 and the encrypted flag are required.

--- xor_properties.py
+++ xor_properties.py
@@ -1,5 +1,4 @@
 from Crypto.Util.number import *
-import base64
 
 """
 Commutative: A ⊕ B = B ⊕ A
@@ -14,29 +13,16 @@ FLAG ^ KEY1 ^ KEY3 ^ KEY2 = 04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf
 """
 
 
-def bd(text):
-    return base64.b64decode(text)
 def bl(data):
     return bytes_to_long(data)
 
 
 k1 = bytes.fromhex("a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313")
-k2 = bytes.fromhex("37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e")
 k3 = bytes.fromhex("c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1")
 fg = bytes.fromhex("04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf")
 
 key1 = bl(k1)
-key2 = key1 ^ bl(k2)
-key3 = key2 ^ bl(k3)
-flag = key1 ^ key2 ^ key3 ^ bl(fg)
+flag = bl(k1) ^ bl(k3) ^ bl(fg)
 
 print(long_to_bytes(flag))
-
-# s1 = bl(k2) ^ bl(k1)
-# s2 = s1 ^ bl(k3)
-# s3 = bl(k1) ^ s1 ^ s2 ^ bl(fg)
-# print(long_to_bytes(s3))
-
-
-
 
